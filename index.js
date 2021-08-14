b323b6680d8736aee831547c88ffebf0ffffba22
Anton Samoilov <absamoilov88@gmail.com>
2021-08-15 01:00:59 +0300

    Refactor index

--- index.js
+++ index.js
@@ -3,22 +3,29 @@ import TelegramBot from "node-telegram-bot-api";
 import handleCancel from "./handlers/cancel.js";
 import handleError from "./handlers/error.js";
 import handleSaveLast from "./handlers/saveLast.js";
-import handleSavePrompt from "./handlers/saveLast.js";
+import handleSavePrompt from "./handlers/savePrompt.js";
 import handleSaveReply from "./handlers/saveReply.js";
 import { handleScenario } from "./handlers/main.js";
 import { isCommand, isCalledDirectlyWithReply } from "./utils/bot.js";
+import { getData, setData } from "./services/appData.js";
+import { getMessageType } from "./utils/message.js";
 
 const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("polling_error", handleError);
-bot.onText(/\/cancel/, handleCancel);
+bot.onText(/\/cancel/, (message) => handleCancel(message, bot));
 bot.onText(/\/save_last/, handleSaveLast);
-bot.onText(/\/save_prompt/, handleSavePrompt);
+bot.onText(/\/save_prompt/, (message) => handleSavePrompt(message, bot));
 bot.on("message", (message) => {
   if (isCommand(message)) return;
-  if (isCalledDirectlyWithReply(message)) handleSaveReply(message);
+  const messageType = getMessageType(message);
 
-  handleScenario(message);
+  if (messageType === "link" || messageType === "file") {
+    setData("lastMessage", message);
+    if (isCalledDirectlyWithReply(message)) handleSaveReply(message);
+  }
+
+  handleScenario(message, bot);
 });
