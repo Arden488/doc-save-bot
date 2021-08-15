04ce8090c8691604f9416b34f69abb97df905905
Anton Samoilov <absamoilov88@gmail.com>
2021-08-15 15:35:15 +0300

    Move bot init to the utils file

--- utils/bot.js
+++ utils/bot.js
@@ -1,12 +1,20 @@
-export function isCommand(message) {
+import TelegramBot from "node-telegram-bot-api";
+
+const token = process.env.TELEGRAM_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+function isCommand(message) {
   return (
     message.entities && message.entities.some((e) => e.type === "bot_command")
   );
 }
 
-export function isCalledDirectlyWithReply(message) {
+function isCalledDirectlyWithReply(message) {
   if (!message.text) return false;
   return /^@DocSaveTestBot+$/.exec(message.text.trim());
   // && msg.reply_to_message &&
   // checkIfLinkOrFile(msg.reply_to_message)
 }
+
+export { isCommand, isCalledDirectlyWithReply, bot };
