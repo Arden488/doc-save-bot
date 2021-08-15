ce19f3f6fb576c19c96b8d5c217e2584ffa34b7c
Anton Samoilov <absamoilov88@gmail.com>
2021-08-16 01:16:06 +0300

    Add save file logic

--- handlers/file.js
+++ handlers/file.js
@@ -1,6 +1,11 @@
-function processFile(document) {
-  console.log("Processing document...");
-  return Promise.resolve();
+import { saveFileToDrive } from "../services/gdrive/index.js";
+import { bot } from "../utils/bot.js";
+
+async function processFile(document) {
+  const fileTmpLink = await bot.getFileLink(document.file_id);
+  const filePersistentLink = await saveFileToDrive(fileTmpLink);
+
+  return filePersistentLink.webContentLink;
 }
 
 export { processFile };
--- handlers/send.js
+++ handlers/send.js
@@ -1,10 +1,24 @@
-import { getData } from "../services/appData.js";
+import { bot } from "../utils/bot.js";
+import { getData, resetData } from "../services/appData.js";
+import { saveToNotion } from "../services/notion.js";
 
-function handleSend() {
+function handleSend(chatId) {
   const description = getData("description");
   const link = getData("link");
 
-  console.log({ description, link });
+  if (saveToNotion({ description, link })) {
+    bot.sendMessage(
+      chatId,
+      "Спасибо, все ушло в базу👌🏻 Да прибудет с тобой понятность"
+    );
+  } else {
+    bot.sendMessage(
+      chatId,
+      "Произошла ошибка. Мы всё записали и скоро поправим. Приносим извинения за неудобства"
+    );
+  }
+
+  resetData();
 }
 
 export { handleSend };
