d7d4fa1dbe851b47dcee5df499c064662a8827ac
Anton Samoilov <absamoilov88@gmail.com>
2021-08-16 01:16:11 +0300

    Add notion service

---
+++ services/notion.js
@@ -0,0 +1,52 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+const databaseId = process.env.NOTION_DB_ID;
+
+async function saveToNotion(data) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        Name: {
+          type: "title",
+          title: [
+            {
+              type: "text",
+              text: {
+                content: data.description,
+              },
+            },
+          ],
+        },
+        Link: {
+          url: data.link,
+        },
+      },
+      //   children: [
+      //     {
+      //       object: "block",
+      //       type: "paragraph",
+      //       paragraph: {
+      //         text: [
+      //           {
+      //             type: "text",
+      //             text: {
+      //               content: JSON.stringify(savedData.link),
+      //             },
+      //           },
+      //         ],
+      //       },
+      //     },
+      //   ],
+    });
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(error.body);
+  }
+}
+
+export { saveToNotion };
