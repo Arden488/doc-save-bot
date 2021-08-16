87ffe67cc3d45a9dd53251b4e762809bcd92b6d6
Anton Samoilov <absamoilov88@gmail.com>
2021-08-16 19:00:18 +0300

    Update google drive service

--- services/gdrive/index.js
+++ services/gdrive/index.js
@@ -2,13 +2,17 @@ import { promises as fs, createReadStream } from "fs";
 import path from "path";
 import readline from "readline";
 import { google } from "googleapis";
+import util from "util";
 import got from "got";
 
 const __dirname = path.resolve();
+const TOKEN_PATH = path.join(__dirname, "services", "gdrive") + "/token.json";
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
+
+let oAuth2Client = null;
 
 async function saveFileToDrive(fileUrl) {
   // If modifying these scopes, delete token.json.
-  const SCOPES = ["https://www.googleapis.com/auth/drive"];
   // The file token.json stores the user's access and refresh tokens, and is
   // created automatically when the authorization flow completes for the first
   // time.
@@ -18,9 +22,9 @@ async function saveFileToDrive(fileUrl) {
     .readFile(path.join(__dirname, "services", "gdrive") + "/credentials.json")
     .catch((err) => console.error("Failed to read credentials file", err));
   const credentials = JSON.parse(credentialsFileRead.toString());
-  const oAuth2Client = await authorize(credentials);
+  await authorize(credentials);
 
-  const fileId = await addRemoteFile(oAuth2Client, fileUrl);
+  const fileId = await addRemoteFile(fileUrl);
   return fileId;
   // (err, content) => {
   //   if (err) return console.log("Error loading client secret file:", err);
@@ -36,31 +40,27 @@ async function saveFileToDrive(fileUrl) {
  * @param {function} callback The callback to call with the authorized client.
  */
 async function authorize(credentials, callback) {
-  const TOKEN_PATH = path.join(__dirname, "services", "gdrive") + "/token.json";
-
   const { client_secret, client_id, redirect_uris } = credentials.web;
-  const oAuth2Client = new google.auth.OAuth2(
+  oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
 
   // Check if we have previously stored a token.
-  const tokenFileRead = await fs.readFile(TOKEN_PATH);
-  // , (err, token) => {
-  //   if (err) return getAccessToken(oAuth2Client, callback);
-  //   oAuth2Client.setCredentials(JSON.parse(token));
-  // callback(oAuth2Client);
-  // });
-  const token = JSON.parse(tokenFileRead.toString());
+  const tokenFileRead = await fs.readFile(TOKEN_PATH).catch(async (err) => {
+    console.log("No saved token");
+  });
 
-  if (token) {
+  if (tokenFileRead) {
+    const token = JSON.parse(tokenFileRead.toString());
+    // const token = tokenRead.res.data;
     await oAuth2Client.setCredentials(token);
+    return;
   } else {
-    await getAccessToken(oAuth2Client);
+    await getAccessToken();
+    return;
   }
-
-  return oAuth2Client;
 }
 
 /**
@@ -70,33 +70,48 @@ async function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 // TODO: rewrite to async await
-async function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
+async function getAccessToken() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question("Enter the code from that page here: ", (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error("Error retrieving access token", err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+  const question = util.promisify(rl.question).bind(rl);
+
+  const authUrl = await oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
   });
+
+  console.log("Authorize this app by visiting this url:", authUrl);
+
+  const answer = await question("Enter the code from that page here: ");
+  try {
+    rl.close();
+    return await processCodeasync(answer);
+  } catch (err) {
+    console.error("Incorrect token creation", err);
+  }
 }
 
-async function addRemoteFile(auth, fileUrl) {
-  const drive = google.drive({ version: "v3", auth });
+async function processCodeasync(code) {
+  const tokenRead = await oAuth2Client
+    .getToken(code)
+    .catch((err) => console.error("Error retrieving access token", err));
+
+  // const token = JSON.parse(tokenRead.toString());
+  const token = tokenRead.res.data;
+  await oAuth2Client.setCredentials(token);
+
+  const tokenFileWrite = await fs
+    .writeFile(TOKEN_PATH, JSON.stringify(token))
+    .catch((err) => console.error(err));
+  if (tokenFileWrite) console.log("Token stored to", TOKEN_PATH);
+
+  return true;
+}
+
+async function addRemoteFile(fileUrl) {
+  const drive = google.drive({ version: "v3", auth: oAuth2Client });
   // const fileUrl =
   //   "https://api.telegram.org/file/bot1897674501:AAFb19ULt0NrHVnaN_Wqi3YseAWgyBaSGOk/documents/file_1.docx";
   //   console.log(request(fileUrl).pipe(fs.createWriteStream("file_1.docx")));
@@ -116,13 +131,11 @@ async function addRemoteFile(auth, fileUrl) {
     media: media,
     fields: "id",
   });
-  return generatePublicUrl(auth, file.data.id);
+  return generatePublicUrl(drive, file.data.id);
 }
 
 //create a public url
-async function generatePublicUrl(auth, fileId) {
-  const drive = google.drive({ version: "v3", auth });
-
+async function generatePublicUrl(drive, fileId) {
   try {
     //change file permisions to public.
     await drive.permissions.create({
