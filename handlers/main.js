0982d2a4543f79a88ab5cc3748a4f50c52254b83
Anton Samoilov <absamoilov88@gmail.com>
2021-08-21 20:43:54 +0300

    Refactor

--- handlers/main.js
+++ handlers/main.js
@@ -1,7 +1,30 @@
-import { getData } from "../services/appData.js";
+import handleSaveReply from "./handlers/saveReply.js";
+import { handleScenario } from "./handlers/main.js";
+import { getData, setData } from "./services/appData.js";
 import handleSaveLast from "./saveLast.js";
 import handleSavePrompt from "./savePrompt.js";
 import handleSaveReply from "./saveReply.js";
+import { getMessageType } from "./utils/message.js";
+import { isCommand, isCalledDirectlyWithReply } from "./utils/bot.js";
+
+function handleMain(message) {
+  (message) => {
+    if (isCommand(message)) return;
+    let targetMessage = message.reply_to_message || message;
+
+    const messageType = getMessageType(targetMessage);
+
+    if (messageType === "link" || messageType === "file") {
+      if (isCalledDirectlyWithReply(message)) {
+        handleSaveReply(targetMessage);
+      } else {
+        setData("lastMessage", targetMessage);
+      }
+    }
+
+    handleScenario(targetMessage);
+  };
+}
 
 function handleScenario(message) {
   const status = getData("status");
@@ -19,4 +42,4 @@ function handleScenario(message) {
   }
 }
 
-export { handleScenario };
+export { handleMain, handleScenario };
--- utils/bot.js
+++ utils/bot.js
@@ -1,16 +1,3 @@
-import TelegramBot from "node-telegram-bot-api";
-
-const token = process.env.TELEGRAM_TOKEN;
-const config = { polling: process.env.NODE_ENV !== "production" };
-
-const bot = new TelegramBot(token, config);
-
-if (process.env.NODE_ENV === "production") {
-  bot.setWebHook(process.env.PUBLIC_URL, {
-    certificate: "../crt.pem", // Path to your crt.pem
-  });
-}
-
 function isCommand(message) {
   return (
     message.entities && message.entities.some((e) => e.type === "bot_command")
@@ -24,4 +11,4 @@ function isCalledDirectlyWithReply(message) {
   // checkIfLinkOrFile(msg.reply_to_message)
 }
 
-export { isCommand, isCalledDirectlyWithReply, bot };
+export { isCommand, isCalledDirectlyWithReply };
