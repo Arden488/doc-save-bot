f857efbd78dd38456026d9b1de8e34f6dd9f2f8d
Arden (tinybox) <mr.madmarker@gmail.com>
2021-08-21 21:18:17 +0300

    Fix log message

--- handlers/cancel.js
+++ handlers/cancel.js
@@ -5,6 +5,6 @@ import { resetData } from "../services/appData.js";
 export default function handleCancel(message) {
   resetData();
 
-  logger.log("info", "Cancel handler", { data: message, step });
+  logger.log("info", "Cancel handler", { data: message });
   bot.sendMessage(message.chat.id, "Ок, всё сбрасываю");
 }
